da4a923d40cbf3f900042c7bf5115d16b2d50838
jmueller734 <jmueller734@fhs.win.fuhsd.org>
2016-11-07 11:12:20 -0800

    Add data files, fix StepCounter

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
 
 public class StepCounter {
+	private static final int WINDOW_LENGTH = 3;
+	private static final int THRESHOLD_LIMIT = 1;
+	private static final int MINIMUM_LIMIT = 2;
+	private static final int THRESHOLD_MULTIPLE=5;
 	public static void main(String[] args) {
-		String[] columnNames={"time", "gyro-x", "gyro-y", "gyro-z"};
-		CSVData test = new CSVData("data/64StepsInPocketJogging-out.csv", columnNames, 1);
+		String[] columnNames={"time", "accel-x", "accel-y", "accel-z"};
+		CSVData test = new CSVData("data/50StepWalkMale.csv", columnNames, 1);
 		test.correctTime(test);
-		System.out.println(countSteps(test.getColumn(1),test.getRows(1,test.getNumRows()-1), 10));
+		System.out.println(countSteps(test.getColumn(1),test.getRows(1,test.getNumRows()-1), WINDOW_LENGTH));
 	}
 	
 	private static int countSteps(double[] times, double[][] sensorData, int windowLength) {
@@ -13,20 +17,24 @@ public class StepCounter {
 		double[] arr = new double[times.length];
 		arr = calculateMagnitudes(sensorData);
 		//arr = NoiseSmoothing.generalRunningAverage(arr, 3);
-		double[] thresholds = calculateWindow(arr, windowLength);
+		double[] thresholds = calculateWindow(arr, WINDOW_LENGTH);
 		for(int i = 1; i < arr.length; i++) {
 			if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
 				if(arr[i] > thresholds[i]) {
 					stepCount++;
-					System.out.println(stepCount +" " + times[i]/1000);
+					System.out.println(stepCount + " " + times[i]);
 				}
 			}
 		}
 		return stepCount;	
 	}	
-
-
 	
+	/***
+	 * Returns array with threshold values
+	 * @param arr
+	 * @param windowLength
+	 * @return
+	 */
 	public static double[] calculateWindow(double[] arr, int windowLength) {
 		double[] result=new double[arr.length];
 		for (int i = 0; i < arr.length; i++){
@@ -34,19 +42,44 @@ public class StepCounter {
 				if(i-windowLength >= 0) {
 					double meanForInterval = calculateMeanInInterval(arr, i-windowLength, i+windowLength);
 					double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, i-windowLength, i+windowLength);
-					result[i] = (meanForInterval+deviationForInterval);
+					if(belowThreshold(arr, WINDOW_LENGTH, i, MINIMUM_LIMIT)){
+						result[i]=((meanForInterval+deviationForInterval))*THRESHOLD_MULTIPLE;
+					}else{
+						result[i] = (meanForInterval+deviationForInterval);
+					}
 				} else {
 					double meanForInterval = calculateMeanInInterval(arr, 0, i+windowLength);
 					double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, 0, i+windowLength);
-					result[i] = (meanForInterval+deviationForInterval);
+					if(belowThreshold(arr, WINDOW_LENGTH, i+WINDOW_LENGTH, MINIMUM_LIMIT)){
+						result[i]=((meanForInterval+deviationForInterval))*THRESHOLD_MULTIPLE;
+					}else{
+						result[i] = (meanForInterval+deviationForInterval);
+					}
 				}
 			} else {
 				double meanForInterval = calculateMeanInInterval(arr, i-windowLength, arr.length);
 				double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, i-windowLength, arr.length);
-				result[i] = (meanForInterval+deviationForInterval);
+				if(belowThreshold(arr, WINDOW_LENGTH, i-WINDOW_LENGTH, MINIMUM_LIMIT)){
+					result[i]=((meanForInterval+deviationForInterval))*THRESHOLD_MULTIPLE;
+				}else{
+					result[i] = (meanForInterval+deviationForInterval);
+				}
 			}
 		}
 		return result;
+	}
+	
+	private static boolean belowThreshold(double[] arr, int windowLength, int index, int threshold) {
+		int underThresholdCounter = 0;
+		int totalIterations = 0;
+		for(int i = index-windowLength; i < index+windowLength; i++) {
+			if(arr[i] < threshold) {
+				underThresholdCounter++;
+			}
+			totalIterations++;
+		}
+		if((double)underThresholdCounter/(double)totalIterations > 0.8) return true;
+		return false;
 	}
 	
 	public static double[] noiseSmoothing(double[] magnitudes, int averageLength) {
